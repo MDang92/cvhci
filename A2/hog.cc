5a2761c7d1b830ad7233b73c0abe3f006a1b2f39
Yang Zhang <yang.zhang@cern.ch>
2016-12-16 18:02:06 +0100

    changed Hog parameters

--- A2/hog.cc
+++ A2/hog.cc
@@ -1,4 +1,7 @@
 #include "hog.h"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
 
 using namespace std;
 
@@ -16,7 +19,11 @@ public:
 /// Constructor
 HOG::HOG()
 {
+    using namespace cv;
 	pimpl = std::shared_ptr<HOGPimpl>(new HOGPimpl());
+    //pimpl->hog.winSize = Size(64, 128);
+    //pimpl->hog.L2HysThreshold = 0.25;
+    pimpl->hog.blockStride = Size(24, 28); // mod 48, 112
 }
 
 /// Destructor
@@ -28,7 +35,7 @@ HOG::~HOG()
 void HOG::startTraining()
 {
 }
-
+    //int writeout = 1;
 /// Add a new training image.
 ///
 /// @param img:  input image
@@ -36,12 +43,29 @@ void HOG::startTraining()
 void HOG::train(const cv::Mat3b& img, bool isPerson)
 {
 	cv::Mat3b img2 = img(cv::Rect((img.cols-64)/2,(img.rows-128)/2,64,128));
+
+    /*
+    if (writeout)
+    {
+        cv::imwrite("data/train/tmp/1.jpg", img2);
+    }
+    */
 	vector<float> vDescriptor;
 	pimpl->hog.compute(img2, vDescriptor);	
-	cv::Mat1f descriptor(1,vDescriptor.size(),&vDescriptor[0]);
+    /*if (writeout)
+    {
+        cout << vDescriptor.size() << endl;
+        for (auto v : vDescriptor)
+            cout << v << " ";
+        cout << endl;
+    }*/
+    cv::Mat1f descriptor(1,vDescriptor.size(),&vDescriptor[0]);
     
 	pimpl->descriptors.push_back(descriptor);
 	pimpl->responses.push_back(cv::Mat1f(1,1,float(isPerson)));
+
+
+    //writeout--;
 }
 
 /// Finish the training.  This finalizes the model.  Do not call
