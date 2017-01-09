02cb5c1dc5b44809bd12563726a1b525c8f8b3a9
Martin Thoma <info@martin-thoma.de>
2017-01-09 18:35:02 +0100

    A3: Online-Score: 0.570645 (use - for euclidean distance)

--- A3/face.cc
+++ A3/face.cc
@@ -1,19 +1,29 @@
 #include "face.h"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-static const int DESCRIPTOR_SIZE = 25600;
-
 struct FACE::FACEPimpl {
-	
-	std::vector<double> pos, neg;
-	std::vector<std::vector<double>> allPos, allNeg;
-	std::vector<std::pair<cv::Point2i,cv::Point2i>> pts;
+    std::vector<double> pos, neg;
+    std::vector<std::vector<double>> allPos, allNeg;
+    std::vector<std::pair<cv::Point2i, cv::Point2i>> pts;
 };
 
+std::vector<cv::Mat> db_0;
+int n_imgs;
+int ind;
+int total;
+cv::PCA pca_0, pca_1, pca_2;
+int numPrincipalComponents = 100;
+vector<int> mean_;
+int width;
 
 /// Constructor
-FACE::FACE() : pimpl(new FACEPimpl()) {
+FACE::FACE() {
 }
 
 /// Destructor
@@ -22,13 +32,26 @@ FACE::~FACE() {
 
 /// Start the training.  This resets/initializes the model.
 void FACE::startTraining() {
-	
-	pimpl->allPos.clear();
-	pimpl->allNeg.clear();
-	pimpl->pts.clear();
-	
-	for (int i=0; i<DESCRIPTOR_SIZE; i++)
-		pimpl->pts.push_back({{rand()%250,rand()%250},{rand()%250,rand()%250}});	
+    n_imgs = 0;
+    ind = 0;
+    db_0.clear();
+}
+
+
+/// Image preprocessing before training / prediction
+cv::Mat preprocess(const cv::Mat3b& img1) {
+    // cv::blur(img1, img1, cv::Size(3, 3));
+
+    // only take the face
+    int r = img1.rows;
+    int c = img1.cols;
+    float border = 1.0 / 10.0;
+    cv::Rect rect(r * border,
+                  c * border,
+                  r - 2 * r * border,
+                  c - 2 * c * border);
+    cv::Mat imga = img1(rect).clone();
+    return imga.reshape(1, 1);
 }
 
 /// Add a new person.
@@ -36,32 +59,56 @@ void FACE::startTraining() {
 /// @param img1:  250x250 pixel image containing a scaled and aligned face
 /// @param img2:  250x250 pixel image containing a scaled and aligned face
 /// @param same: true if img1 and img2 belong to the same person
+
 void FACE::train(const cv::Mat3b& img1, const cv::Mat3b& img2, bool same) {
-	
-	std::vector<double> desc;
-	for (auto &p : pimpl->pts)
-		desc.push_back( ((img1(p.first)[1]<img1(p.second)[1]) == (img2(p.first)[1]<img2(p.second)[1])) - .5);
-		
-	if (same)
-		pimpl->allPos.push_back(desc);
-	else 
-		pimpl->allNeg.push_back(desc);	
+    if (n_imgs >= 200)  // speed things up
+    {
+        return;
+    }
+
+    db_0.push_back(preprocess(img1));
+    db_0.push_back(preprocess(img2));
+
+    n_imgs += 2;
+    total = db_0[0].rows * db_0[0].cols;
 }
+
 
 /// Finish the training.  This finalizes the model.  Do not call
 /// train() afterwards anymore.
 void FACE::finishTraining() {
-	
-	pimpl->pos = vector<double>(DESCRIPTOR_SIZE,0);
-	for (auto &d : pimpl->allPos)
-		for (uint i=0; i<d.size(); i++) 
-			pimpl->pos[i] += d[i]/pimpl->allPos.size();
+    cv::Mat mat_0(total, n_imgs, CV_32FC1);
+    mean_.reserve(total);
+    width = db_0[0].rows;
 
-	pimpl->neg = vector<double>(DESCRIPTOR_SIZE,0);
-	for (auto &d : pimpl->allNeg)
-		for (uint i=0; i<d.size(); i++) 
-			pimpl->neg[i] += d[i]/pimpl->allNeg.size();
+    // Calculate mean
+    for (unsigned int i = 0; i < db_0.size(); i++) {
+        for (int j = 0; j < total; ++j)
+        {
+            // Compute mean
+            mean_[j] += double(db_0[i].at<uchar>(0, j)) / db_0.size();
+        }
+    }
+
+
+    // Subtract mean and add matrix for PCA
+    for (unsigned int i = 0; i < db_0.size(); i++) {
+        for (int j = 0; j < total; ++j)
+        {
+            db_0[i].at<uchar>(0, j) -= mean_[j];
+        }
+        db_0[i].reshape(1, total).copyTo(mat_0.col(i));
+    }
+    pca_0 = cv::PCA(mat_0, cv::Mat(), CV_PCA_DATA_AS_COL, numPrincipalComponents);
+
+    // Export computed mean , eigen vectors & eigen values Matrix
+    /*    cv::FileStorage fs("export.dat", cv::FileStorage::WRITE);
+        fs << "mean" << pca_0.mean;
+        fs << "eigenvectors" << pca_0.eigenvectors;
+        fs << "eigenvalues" << pca_0.eigenvalues;
+        fs.release();*/
 }
+
 
 /// Verify if img corresponds to the provided name.  The result is a floating point
 /// value directly proportional to the probability of being correct.
@@ -70,20 +117,29 @@ void FACE::finishTraining() {
 /// @param img2:  250x250 pixel image containing a scaled and aligned face
 /// @return:    similarity score between both images
 double FACE::verify(const cv::Mat3b& img1, const cv::Mat3b& img2) {
-	
-//	return rand()%256;
-//	return -cv::norm(img1-img2);
+    cv::Mat img_flat_a = preprocess(img1);
+    cv::Mat img_flat_b = preprocess(img2);
 
-	std::vector<double> desc;
-	for (auto &p : pimpl->pts)
-		desc.push_back( ((img1(p.first)[1]<img1(p.second)[1]) == (img2(p.first)[1]<img2(p.second)[1])) - .5);
+    cv::Mat test_a0(numPrincipalComponents, 1, CV_32FC1);
+    cv::Mat test_b0(numPrincipalComponents, 1, CV_32FC1);
+    cv::Mat compress_a0(numPrincipalComponents, 1, CV_32FC1);
+    cv::Mat compress_b0(numPrincipalComponents, 1, CV_32FC1);
 
-	double scorePos=0, scoreNeg=0;
-	for (uint i=0; i<desc.size(); i++) {
-		scorePos += pimpl->pos[i]*desc[i];
-		scoreNeg += pimpl->neg[i]*desc[i];
-	}
+    // Subtract mean
+    for (int j = 0; j < total; ++j)
+    {
+        img_flat_a.at<uchar>(0, j) -= mean_[j];
+        img_flat_b.at<uchar>(0, j) -= mean_[j];
+    }
 
-	return scorePos/(scoreNeg+1E-10);
+    img_flat_a.reshape(1, total).copyTo(test_a0);
+    img_flat_b.reshape(1, total).copyTo(test_b0);
+
+    pca_0.project(test_a0, compress_a0.col(0));
+    pca_0.project(test_b0, compress_b0.col(0));
+
+    // Cosine distance
+    //return compress_a0.dot(compress_b0) / (cv::norm(compress_a0, cv::NORM_L2) * cv::norm(compress_b0, cv::NORM_L2));
+    return -cv::norm(compress_a0 - compress_b0, cv::NORM_L2);
 }
 
